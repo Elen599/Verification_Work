59b3b5d7190eeaa086f46a688a87462e087f5de1
«Elena <helenvit599@gmail.com>
2022-09-16 22:37:31 +0500

    Added function 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
+// Функция подсчета подходящих элементов
+int CountElement (string[] array)
+{
+    int size = 0, temp = 0;
+    string box = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        box = array[i];
+        temp = box.Length;
+        if (temp < 4)
+            size ++;
+    }
+    return size;
+}
+
 // Код программы
 string[] originalArray = {"print","hello","car","|:(","><"};
 int sizeArray = CountElement(originalArray);
